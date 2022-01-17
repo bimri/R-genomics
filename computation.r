bfd4b67e9edf46a4fcf643715708110bca939035
bimri <bimri@outlook.com>
2022-01-17 03:53:04 +0300

    Computation in R

---
+++ computation.r
@@ -0,0 +1,9 @@
+2 + 3 * 5       # Note the order of operations
+log(10)         # Natural logarithm with base e
+5^2             # 5 raised to the second power
+3/2             # Division
+sqrt(16)        # Square root
+abs(3-7)        # Absolute value of 3-7
+pi              # the number
+exp(2)          # exponential function
+# This is a comment line
