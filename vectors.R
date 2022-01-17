6cc066c36d3804a0bc4f9481825971c797cbd566
bimri <bimri@outlook.com>
2022-01-17 04:22:03 +0300

    Data Structures: Vectors

---
+++ vectors.R
@@ -0,0 +1,45 @@
+# Create a vector named x with 5 elements
+x <-c(1,3,2,10,5)
+x
+
+x = c(1,3,2,10,5)
+x
+
+
+# create a vector of consecutive integers y
+y<-1:10
+y
+ 
+#scalar addition
+y+2
+
+#scalar multiplication
+2*y
+
+#raise each component to the second power
+y^2
+
+#raise 2 to the first through tenth power
+2^y
+
+# y itself has not been unchanged
+y
+
+# it is now changed
+y<-y*2
+y
+
+
+# create a vector of 1s,length 3
+r1<-rep(1,3)
+length(r1)
+
+# class of the vector
+class(r1)                
+
+# this is actually a vector length one
+a<-1                     
+a
+
+
+# "The standardassignmentoperatorinRis <-."
